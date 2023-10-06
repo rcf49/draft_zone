7ef6b987852dbc33531c8249d286ee4519609361
rcf49 <rcf49@drexel.edu>
2023-10-05 22:14:04 -0400

    Finishing up testing GitHub Actions

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,8 @@
 # test GitHub actions
 
+from datetime import datetime
+
 print("Hello, this is a test print from Python.")
 
 with open("test.txt", "w") as file:
-    file.write("Hello, this is a test write from Python.")
+    file.write(f"Hello, this is a test write from Python. {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
